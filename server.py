22c24463c64b4376b6484815331f9289698179b9
benjamin-fuerst <105423019+benjamin-fuerst@users.noreply.github.com>
2022-11-05 14:23:16 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import whisper
 
 if __name__ == "__main__":
     ip = "127.0.0.1"
@@ -8,12 +9,14 @@ if __name__ == "__main__":
     server.bind((ip, port))
     server.listen(5) #up to 5 connections
     
+    model = whisper.load_model("tiny")
+    
     while True:
         client, address = server.accept()
         print(f"Connection Established - {address[0]}:{address[1]}")
         
-        string = client.recv(1024) #number of bytes to receive
-        string = string.decode("utf-8")
-        string = string.upper()
-        client.send(bytes(string, "utf-8"))
+        filepath = client.recv(1024) #number of bytes to receive
+        filepath = filepath.decode("utf-8")
+        result = model.transcribe(filepath)
+        client.send(bytes(result["text"], "utf-8"))
         client.close()
