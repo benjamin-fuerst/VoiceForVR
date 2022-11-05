5470ae4d14e6b397a5fe58ed0a7c69595de4d7ae
benjamin-fuerst <105423019+benjamin-fuerst@users.noreply.github.com>
2022-11-05 14:22:41 +0100

    Update client.py

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.connect((ip, port))
     
-    string = input("Enter string: ")
+    string = input("Enter filename: ")
     server.send(bytes(string, "utf-8"))
     buffer = server.recv(1024)
     buffer = buffer.decode("utf-8")
