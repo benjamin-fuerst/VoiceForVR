c72d168b8b13751cc13e1674f626198c3b28e429
Daniel <i@dast.xyz>
2022-11-07 17:26:23 +0100

    switch to flask

--- README.md
+++ README.md
@@ -3,6 +3,5 @@ To run the scripts, use two command lines and run
 $python server.py  
 $python client.py  
 or, depending on installation.  
-$python3 server.py  
-$python3 client.py  
+`python3 -m flask --app http_server run`
 From there, client takes filepath as input, sends it, server reads the file, transcribes it, sends it back, client displays it.  
--- src/http_server.py
+++ src/http_server.py
@@ -1,44 +1,24 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import time
 import whisper
-from io import BytesIO
+from flask import Flask, request
 
 HOST = "127.0.0.1"
 PORT = 8000
 
-class WhisperHTTP(BaseHTTPRequestHandler):
-    
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        
-        self.wfile.write(bytes("<html><body><h1>HELLO WORLD</h1></body></html>", "utf-8"))
-    
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        filepath = self.rfile.read(content_length)
-        filepath = filepath.decode("utf-8")
-        result = model.transcribe(filepath)
-        
-        self.send_response(200)
-        self.end_headers()
-        
-        response = f"This is a POST request. Received: {result['text']}"
-        
-        self.wfile.write(bytes(response, "utf-8"))
-        
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    model = whisper.load_model("tiny")
-    
-    server = HTTPServer((HOST, PORT), WhisperHTTP)
-    print(f"Server  running at http://{HOST}:{PORT}")
-    
-    try:
-        server.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    
-    server.server_close()
-    print("Server stopped")
+model = whisper.load_model("tiny")
+
+
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
+    path = request.get_json(True)['path']
+    print(path)
+    result = model.transcribe(path)
+    print(result['text'])
+    return {
+        "text": result['text']
+    }
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
